529ddd1b19cf4ac6e421c8f10e6ecbc0c2c6b797
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-10-09 17:36:10 +0100

    style: add significa/svelte-ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
+<script lang="ts">
+	import { Button, Link } from "@significa/svelte-ui";
+</script>
+
 <main class="flex h-screen flex-col items-center justify-center">
 	<h1 class="text-3xl font-bold">SvelteKit Starter Template</h1>
-	<a
-		href="https://github.com/ibrahimraimi/svelte-starter"
-		class="mt-2 rounded-full border border-slate-700 p-2 hover:bg-slate-700 hover:text-white"
-		>GitHub Repo</a
-	>
+
+	<Button class="my-5">
+		<Link href="https://github.com/ibrahimraimi/svelte-starter" class="py-5">GitHub Repo</Link>
+	</Button>
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,35 @@
 /** @type {import('tailwindcss').Config}*/
 const config = {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
-
+	content: [
+		"./src/**/*.{html,js,svelte,ts}",
+		"./node_modules/@significa/svelte-ui/**/*.{html,js,svelte,ts}"
+	],
+	presets: [
+		require("@significa/svelte-ui/tailwind-preset")({
+			fonts: {
+				sans: {
+					name: "Significa Sans",
+					fontFaces: [
+						{
+							fontWeight: "400",
+							src: `url('/fonts/significa-regular.woff2') format('woff2')`,
+							ascentOverride: "95%"
+						},
+						{
+							fontWeight: "500",
+							src: `url('/fonts/significa-medium.woff2') format('woff2')`,
+							ascentOverride: "95%"
+						},
+						{
+							fontWeight: "600",
+							src: `url('/fonts/significa-semibold.woff2') format('woff2')`,
+							ascentOverride: "95%"
+						}
+					]
+				}
+			}
+		})
+	],
 	theme: {
 		extend: {}
 	},
